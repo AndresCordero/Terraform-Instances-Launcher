153fd1fda3b82f187066f9dd73597894281152b6
AndresCordero <an.cornmo@gmail.com>
2025-08-24 22:43:45 -0600

    creaing nginx with code

--- main.tf
+++ main.tf
@@ -4,8 +4,6 @@ provider "aws" {
     region = "us-east-1"
 }
 
-
-
 data "aws_ssm_parameter" "amzn2" {
   name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
 }
@@ -13,8 +11,15 @@ data "aws_ssm_parameter" "amzn2" {
 
 resource "aws_instance" "nginx-server" {
     ami = data.aws_ssm_parameter.amzn2.value
-    instance_type = "t3.micro"
-}
+    instance_type = "t3.micro"  
 
+  user_data = <<-EOF
+#!/bin/bash
+sudo yum update -y
+sudo amazon-linux-extras install nginx1 -y
+sudo systemctl start nginx
+sudo systemctl enable nginx
+EOF
+} 
 
 
